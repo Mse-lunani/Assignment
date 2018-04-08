2400c42eb16ab89733f96f74e0190964197a192c
Mse-lunani <35915767+Mse-lunani@users.noreply.github.com>
2018-04-08 13:59:51 +0300

    new commit

---
+++ src/assignment/array.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment;
+
+/**
+ *
+ * @author LENOVO V310
+ */
+public class array {
+    public void Array(){
+        int []count= new int[10];
+        for(int i=0;i<10;i++){
+            count[i]=0;
+            count[i]=+1;
+            System.out.print("\t"+count[i]);
+            
+        }
+        
+    }
+}
