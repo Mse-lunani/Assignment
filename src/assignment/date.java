49782e901778782f8f2c6c0fc82bacefe160d619
Mse-lunani <35915767+Mse-lunani@users.noreply.github.com>
2018-04-08 13:01:21 +0300

    new commit

--- src/assignment/date.java
+++ src/assignment/date.java
@@ -10,19 +10,23 @@ package assignment;
  * @author LENOVO V310
  */
 public class date {
+    // these are the instance variables
     int month;
     int date;
     int year;
+    //this is the constructor
  public date(int mnth,int dte,int yr){
      month= mnth;
      date= dte;
      year=yr;
    }
+ // this is setting of the month date and years
  public void set(int month1,int year1,int date1){
      month=month1;
      date=date1;
      year=year1;
  }
+ //this is the display section
  public void displayMonth(){
       System.out.println(month+"/"+date+"/"+year);
  }
